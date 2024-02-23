a07b49977dcc76882ddcf50968641519c8c675a7
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-02-23 14:55:47 -0300

    s

--- exercicio1/Carrinho (Alunos)/js/script.js
+++ exercicio1/Carrinho (Alunos)/js/script.js
@@ -15,38 +15,38 @@ modelsJson.map((item, index)=>{
 
     // Atualizar os dados do item do modelo clonado
 
-modelsItem.setAttribute('data-key', index);
-modelsItem.querySelector('.models-item--img img').src = item.img;
-modelsItem.querySelector('.models-item--price').innerHTML =` R$ ${item.price[2].toFixed(3)}`;
-modelsItem.querySelector('.model-item--name').innerHTML = item.name;
-modelsItem.querySelector('.models-item--desc').innerHTML = item.description;
+    modelsItem.setAttribute('data-key', index);
+    modelsItem.querySelector('.models-item--img img').src = item.img;
+    modelsItem.querySelector('.models-item--price').innerHTML =` R$ ${item.price[2].toFixed(3)}`;
+    modelsItem.querySelector('.model-item--name').innerHTML = item.name;
+    modelsItem.querySelector('.models-item--desc').innerHTML = item.description;
 
-// Adicionar evento de clique para exibir o modal ao clicar no modelo
-modelsItem.querySelector('a').addEventListener('click', (e)=> {
-    e.preventDefault();
-    key = e.target.closest('.models-item').getAttribute('data-key');
-    modalQt= 1;
+    // Adicionar evento de clique para exibir o modal ao clicar no modelo
+    modelsItem.querySelector('a').addEventListener('click', (e)=> {
+        e.preventDefault();
+        key = e.target.closest('.models-item').getAttribute('data-key');
+        modalQt= 1;
 
-    c('.modelsBig img').src = modelsJson[key].img;
-    c('.modelsInfo h1').innerHTML = modelsJson[key].name;
-    c('.modelsInfo--desc').innerHTML = modelsJson[key].description;
-    c('.modelsInfo--size.selected').classList.remove('selected');
+        c('.modelsBig img').src = modelsJson[key].img;
+        c('.modelsInfo h1').innerHTML = modelsJson[key].name;
+        c('.modelsInfo--desc').innerHTML = modelsJson[key].description;
+        c('.modelsInfo--size.selected').classList.remove('selected');
 
-    // Atualizar os tamanhos disponíveis no modal
-    cs('.modelsInfo--size').forEach((size, sizeIndex) =>{
-        if (sizeIndex == 2){
-            size.classList.add('selected');
-            c('.modelsInfo--actualPrice').innerHTML = `R$ ${modelsJson[key].price[sizeIndex].toFixed(2)}`;
-        }
-        size.querySelector('span').innerHTML = modelsJson[key].sizes[sizeIndex];
-    });
+        // Atualizar os tamanhos disponíveis no modal
+        cs('.modelsInfo--size').forEach((size, sizeIndex) =>{
+            if (sizeIndex == 2){
+                size.classList.add('selected');
+                c('.modelsInfo--actualPrice').innerHTML = `R$ ${modelsJson[key].price[sizeIndex].toFixed(2)}`;
+            }
+            size.querySelector('span').innerHTML = modelsJson[key].sizes[sizeIndex];
+        });
 
-    c('.modelsInfo--qt').innerHTML = modalQt;
-    c('modelsWindowArea').style.opacity = 0;
-    c('.modelsWindowArea').style.display = 'flex';
-    setTimeout(()=> {
-        c('.modelsWindowArea').styl.opacity = 1;
-    }, 200);
+        c('.modelsInfo--qt').innerHTML = modalQt;
+        c('modelsWindowArea').style.opacity = 0;
+        c('.modelsWindowArea').style.display = 'flex';
+        setTimeout(()=> {
+            c('.modelsWindowArea').style.opacity = 1;
+        }, 200);
 
     });
     c('.models-area').append(modelsItem);
