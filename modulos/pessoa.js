97d2d46e23b6031c655a20b2fa74959ce2d2c1fb
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-02-23 07:30:13 -0300

    Arquivos das aulas

---
+++ modulos/pessoa.js
@@ -0,0 +1,16 @@
+// Objeto no JS
+// const pessoa = {
+export const pessoa = {
+    nome : 'Ignacio',
+    idade : 41,
+    cidade : "São Paulo"
+}
+
+// const curso = {
+export const curso = {
+    titulo : 'HTML e CSS',
+    cargaHoraria : 40,
+}
+
+// export default pessoa;
+// export {pessoa, curso};
